7d99939d8360be88a82ec46eb57e47eef466bed8
William Gerald Blondel <contact@williamblondel.me>
2016-06-12 09:19:55 +0200

    Fixed: Typo in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='pycdiscount',
-      version='0.3',
+      version='0.3.1',
       description='A simple Python Wrapper for CDiscount API using Requests.',
       url='https://github.com/wblondel/PythonCdiscountWrapper',
       author='William Gerald Blondel',
       author_email='contact@williamblondel.me',
       install_requires=["requests"],
-      include_package_date=True,
+      include_package_data=True,
       license='GNU GPL',
       packages=['pycdiscount'],
       classifiers=[
