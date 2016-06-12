3cee92510680c93c0a2c0a564270a43816861e57
William Gerald Blondel <contact@williamblondel.me>
2016-06-12 09:15:04 +0200

    Changing version to solve PyPi issue

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
 setup(name='pycdiscount',
-      version='0.2',
+      version='0.3',
       description='A simple Python Wrapper for CDiscount API using Requests.',
       url='https://github.com/wblondel/PythonCdiscountWrapper',
       author='William Gerald Blondel',
       author_email='contact@williamblondel.me',
+      install_requires=["requests"],
+      include_package_date=True,
       license='GNU GPL',
       packages=['pycdiscount'],
+      classifiers=[
+          "Programming Language :: Python",
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3.4",
+          "Topic :: Internet",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+      ],
       zip_safe=False)
